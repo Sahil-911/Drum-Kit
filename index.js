556497b112335b56d05e6c0e179c766297e5e9e6
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-08-11 14:01:48 +0530

    add JS

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const key = document.querySelector('.key');
+removeTransition(e)
+{
+    if(e.propertyName!=='transform')
+        return;
+    e.target.classList.remove('playing');
+}
 
-// document.addEventListener('keydown',(e)=>{
-//     if(e.key==65)
+playSound(e)
+{
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    const key = document.querySelector(`div[data-key='${e.keyCode}']`);
+    if(!audio)
+        return;
 
-// });
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+    setTimeout(() => key.classList.remove('playing'),100);
+}
+
+const keys = array.from(document.querySelector('.key'));
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+window.addEventListener('keydown',playSound);
